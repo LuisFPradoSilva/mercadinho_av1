1bb62c73ffdd7d44bbd02b65ac9a087bb75f7504
LuisFPradoSilva <luisfelipepradodasilva53@gmail.com>
2024-09-10 19:39:02 -0300

    Adicionando annotation @EntityScan na classe principal

--- src/main/java/com/avaliacao/mercadinho/MercadinhoApplication.java
+++ src/main/java/com/avaliacao/mercadinho/MercadinhoApplication.java
@@ -2,7 +2,9 @@ package com.avaliacao.mercadinho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"com.avaliacao.domains", "com.avaliacao.domains.enums"})
 @SpringBootApplication
 public class MercadinhoApplication {
 
