081b4bf7c85095d28641ad47ebf7f1f24b65753b
LuisFPradoSilva <luisfelipepradodasilva53@gmail.com>
2024-09-11 18:35:10 -0300

    Tratando exceção Bad Request ao inserir dados inválidos na busca de um cadastro

--- src/main/java/com/avaliacao/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/avaliacao/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.avaliacao.services.exceptions.ObjectNotFoundException;
 
@@ -18,5 +19,13 @@ public class ResourceExceptionHandler {
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Object not found", ex.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<StandardError> handlerMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex, HttpServletRequest request) {
+
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Bad Request", ex.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
