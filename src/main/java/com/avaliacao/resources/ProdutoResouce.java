8a47de94a7f97c394413b0ac88e6a72a68d65ae7
LuisFPradoSilva <luisfelipepradodasilva53@gmail.com>
2024-09-11 17:30:42 -0300

    Implementando busca por ID para Produto

--- src/main/java/com/avaliacao/resources/ProdutoResouce.java
+++ src/main/java/com/avaliacao/resources/ProdutoResouce.java
@@ -3,9 +3,11 @@ package com.avaliacao.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.avaliacao.domains.Produto;
 import com.avaliacao.domains.dtos.ProdutoDTO;
 import com.avaliacao.services.ProdutoService;
 
@@ -21,5 +23,11 @@ public class ProdutoResouce {
     @GetMapping
     public ResponseEntity<List<ProdutoDTO>> findAll() {
         return ResponseEntity.ok().body(produtoService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProdutoDTO> findById(@PathVariable int id) {
+        Produto obj = this.produtoService.findById(id);
+        return ResponseEntity.ok().body(new ProdutoDTO(obj));
     }
 }
--- src/main/java/com/avaliacao/services/ProdutoService.java
+++ src/main/java/com/avaliacao/services/ProdutoService.java
@@ -3,10 +3,12 @@ package com.avaliacao.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.avaliacao.domains.Produto;
 import com.avaliacao.domains.dtos.ProdutoDTO;
 import com.avaliacao.repositories.ProdutoRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,5 +19,10 @@ public class ProdutoService {
 
     public List<ProdutoDTO> findAll() {
         return produtoRepo.findAll().stream().map(obj -> new ProdutoDTO(obj)).collect(Collectors.toList());
+    }
+
+    public Produto findById(int id) {
+        Optional<Produto> obj = produtoRepo.findById(id);
+        return obj.orElse(null);
     }
 }
