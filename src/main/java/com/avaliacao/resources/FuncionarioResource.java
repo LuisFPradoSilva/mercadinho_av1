8a38fef5e7d9730e4891c8009db2a2d20e4c5446
LuisFPradoSilva <luisfelipepradodasilva53@gmail.com>
2024-09-11 17:25:12 -0300

    Implementando busca por ID para Funcionario

--- src/main/java/com/avaliacao/resources/FuncionarioResource.java
+++ src/main/java/com/avaliacao/resources/FuncionarioResource.java
@@ -3,9 +3,11 @@ package com.avaliacao.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.avaliacao.domains.Funcionario;
 import com.avaliacao.domains.dtos.FuncionarioDTO;
 import com.avaliacao.services.FuncionarioService;
 
@@ -21,5 +23,11 @@ public class FuncionarioResource {
     @GetMapping
     public ResponseEntity <List<FuncionarioDTO>> findAll() {
         return ResponseEntity.ok().body(funcionarioService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<FuncionarioDTO> findById(@PathVariable Long id) {
+        Funcionario obj = this.funcionarioService.findById(id);
+        return ResponseEntity.ok().body(new FuncionarioDTO(obj));
     }
 }
--- src/main/java/com/avaliacao/services/FuncionarioService.java
+++ src/main/java/com/avaliacao/services/FuncionarioService.java
@@ -3,10 +3,12 @@ package com.avaliacao.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.avaliacao.domains.Funcionario;
 import com.avaliacao.domains.dtos.FuncionarioDTO;
 import com.avaliacao.repositories.FuncionarioRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,4 +20,9 @@ public class FuncionarioService {
     public List<FuncionarioDTO> findAll() {
         return funcionarioRepo.findAll().stream().map(obj -> new FuncionarioDTO(obj)).collect(Collectors.toList());
     }
+
+    public Funcionario findById(Long id) {
+        Optional<Funcionario> obj = funcionarioRepo.findById(id);
+        return obj.orElse(null);
+    } 
 }
