6017a2344d02a92a169285cf3282435639b91c25
LuisFPradoSilva <luisfelipepradodasilva53@gmail.com>
2024-09-12 19:05:58 -0300

    Adicionando anotações do Swagger na classe ProdutoResource

--- src/main/java/com/avaliacao/resources/ProdutoResouce.java
+++ src/main/java/com/avaliacao/resources/ProdutoResouce.java
@@ -16,6 +16,8 @@ import com.avaliacao.domains.Produto;
 import com.avaliacao.domains.dtos.ProdutoDTO;
 import com.avaliacao.services.ProdutoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import java.net.URI;
@@ -23,22 +25,26 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/produto")
+@Tag(name = "Produto", description = "API para gerenciamento dos produtos")
 public class ProdutoResouce {
 
     @Autowired
     private ProdutoService produtoService;
 
+    @Operation(summary = "Listar todos os produtos", description = "Retorna uma lista com todos os produtos cadastrados")
     @GetMapping
     public ResponseEntity<List<ProdutoDTO>> findAll() {
         return ResponseEntity.ok().body(produtoService.findAll());
     }
 
+    @Operation(summary = "Buscar produto por ID", description = "Retorna o produto correspondente ao ID fornecido")
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProdutoDTO> findById(@PathVariable int id) {
         Produto obj = this.produtoService.findById(id);
         return ResponseEntity.ok().body(new ProdutoDTO(obj));
     }
 
+    @Operation(summary = "Criar um novo produto", description = "Cria um novo produto com base nos dados inseridos")
     @PostMapping
     public ResponseEntity<ProdutoDTO> create(@Valid @RequestBody ProdutoDTO objDto) {
         Produto newObj = produtoService.create(objDto);
@@ -46,12 +52,14 @@ public class ProdutoResouce {
         return ResponseEntity.created(uri).build();
     }
 
+    @Operation(summary = "Atualizar produto", description = "Atualiza os dados do produto com base no ID fornecido")
     @PutMapping(value = "/{id}")
     public ResponseEntity<ProdutoDTO> update(@PathVariable int id, @Valid @RequestBody ProdutoDTO onjDto) {
         Produto obj = produtoService.update(id, onjDto);
         return ResponseEntity.ok().body(new ProdutoDTO(obj));
     }
 
+    @Operation(summary = "Deletar produto", description = "Deleta o produto correspondente ao ID fornecido")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<ProdutoDTO> delete(@PathVariable int id) {
         produtoService.delete(id);
